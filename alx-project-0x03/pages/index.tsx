bbb38544128614c697d93198bf47a8dedf92937e
faithakinbobola <faithakinbobola2@gmail.com>
2025-07-28 13:57:29 +0100

    update

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   const router = useRouter();
 
   // Imeperative routing with useRouter
-  const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
-    router.push(pageRoute, undefined, { shallow: false})
+  const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false })
   }
 
   return (
@@ -22,8 +22,8 @@ export default function Home() {
       {/* Navigation Options */}
       <div className="flex gap-6">
         <Button action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} buttonLabel="Generate Text" buttonBackgroundColor="blue" />
-        <Button action={() => routeToNextPage({ pageRoute: '/text-to-image'})} buttonLabel="Text to Image" buttonBackgroundColor="green" />
-        <Button action={() => routeToNextPage({ pageRoute: '/counter-app'})} buttonLabel="Contact us" buttonBackgroundColor="orange" />
+        <Button action={() => routeToNextPage({ pageRoute: '/text-to-image' })} buttonLabel="Text to Image" buttonBackgroundColor="green" />
+        <Button action={() => routeToNextPage({ pageRoute: '/counter-app' })} buttonLabel="Contact us" buttonBackgroundColor="orange" />
       </div>
     </div>
   );
